f192fd3775dd15813935135bd80c1f664f82ad54
jonschlinkert <jon.schlinkert@sellside.com>
2016-08-02 06:51:48 -0400

    add async loading tests

--- suite/test/templates/app.onLoad.js
+++ suite/test/templates/app.onLoad.js
@@ -26,6 +26,67 @@ module.exports = function(App, options, runner) {
 
         collection.addView('whatever', {path: 'blog/foo.js', content: 'bar baz'});
       });
+
+      it('should support async loading', function(cb) {
+        var collection = app.collection();
+
+        app.onLoad('blog/:title', function(view, next) {
+          assert.equal(view.path, 'blog/foo.js');
+          next();
+        });
+
+        var obj = {path: 'blog/foo.js', content: 'bar baz'};
+        collection.addView('whatever', obj, function(err, view) {
+          if (err) return cb(err);
+          cb();
+        });
+      });
+
+      it('should expose the view in the callback', function(cb) {
+        var collection = app.collection();
+
+        var obj = {path: 'blog/foo.js', content: 'bar baz'};
+        collection.addView('whatever', obj, function(err, view) {
+          if (err) return cb(err);
+          assert(view);
+          assert(view.path);
+          cb();
+        });
+      });
+
+      it('should run `.onLoad` middleware before calling the callback', function(cb) {
+        var collection = app.collection();
+
+        app.onLoad(/blog/, function(view, next) {
+          view.title = 'foo';
+          next();
+        });
+
+        var obj = {path: 'blog/foo.js', content: 'bar baz'};
+        collection.addView('whatever', obj, function(err, view) {
+          if (err) return cb(err);
+          assert.equal(view.title, 'foo');
+          cb();
+        });
+      });
+
+      it('should be async', function(cb) {
+        var collection = app.collection();
+
+        app.onLoad(/blog/, function(view, next) {
+          view.title = 'foo';
+          setTimeout(function() {
+            next();
+          }, 20);
+        });
+
+        var obj = {path: 'blog/foo.js', content: 'bar baz'};
+        collection.addView('whatever', obj, function(err, view) {
+          if (err) return cb(err);
+          assert.equal(view.title, 'foo');
+          cb();
+        });
+      });
     });
 
     describe('view collections', function() {
